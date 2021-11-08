7a35af1b5ce1bc204f6c647f8e794fdbe927a7a6
ALEXANDER <msumba83@gmail.com>
2021-11-08 13:33:26 +0200

    done Route

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-      res.send("<h1>Alander is learning express.js</h1>")
+      res.send("<h1>Aexander is learning express.js</h1>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("contact Me At: msumba83@gmail.com");
+});
+
+app.get("/about", function(req, res)  {
+    res.send("<h1>Welcome to Alexander's Website</h1>");
+});
+
+app.get("/hobbies", function(req, res) {
+    res.send("<ul><li>Cofee</li><li>Tea</li><li>Wine</li></ul>")
 })
 
 
